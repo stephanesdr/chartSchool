9561607dc1a2e706dafe99e3527dd78b9097ba0d
stephanesdr <stadlerstephane@gmail.com>
2019-05-14 00:34:21 +0200

    add support folder to /spec and some setting to rails helper for shoulda and devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,16 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+
+
+require 'faker'
+require 'rspec/rails'
+require 'devise'
+require 'simplecov'
+SimpleCov.start
+
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -60,4 +70,18 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
+
+  # For Devise > 4.1.1
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  # Use the following instead if you are on Devise <= 4.1.1
+  # config.include Devise::TestHelpers, :type => :controller
+  config.extend ControllerMacros, type: :controller
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
---
+++ spec/support/controller_maccros.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module ControllerMacros
+    def login_user
+      before do
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        user = FactoryBot.create(:user)
+        # user.confirm! dont need this until user has to validates his account
+        sign_in user
+      end
+    end
+  
+    def login_admin
+      before do
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        user = FactoryBot.create(:user)
+        user.admin = true
+        user.save
+        # user.confirm! dont need this until user has to validates his account
+        sign_in user
+      end
+    end
+  end
+  
