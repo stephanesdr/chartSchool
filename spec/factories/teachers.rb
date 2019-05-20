59d9d518769a9b60276f69dcc71c606949d290f3
redcozmo <redcozmo@hotmail.com>
2019-05-20 19:37:54 +0200

    add tests on Teacher model

--- spec/factories/teachers.rb
+++ spec/factories/teachers.rb
@@ -26,5 +26,10 @@
 
 FactoryBot.define do
   factory :teacher do
+    name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { "password" }
+    password_confirmation { "password" }
+    confirmed_at { Time.zone.today }
   end
 end
