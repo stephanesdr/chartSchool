28d556f91d9eb2b799f27c47f8feec2e9dead53a
AlexDNZ <paleozene@gmail.com>
2019-05-20 16:40:31 +0200

    Course Table : Creation of the model Set validation for title and date attributes Set instanciation tests Set courses factory generator

---
+++ app/models/course.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: courses
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  end_time    :datetime
+#  resource    :text
+#  start_time  :datetime
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Course < ApplicationRecord
+  validates :title, presence: true
+  validate :date_cannot_be_in_the_past
+
+  def date_cannot_be_in_the_past
+    if end_time.present? && end_time < Time.zone.today
+      errors.add(:end_time, "end_date can't be in the past")
+    elsif start_time.present? && start_time < Time.zone.today
+      errors.add(:start_time, "start_date can't be in the past")
+    end
+  end
+end
