e4b90fe3a1939f548e9d199ea54b771a537c905f
MargVander <margauxvanderbeken@gmail.com>
2019-05-20 21:36:08 +0200

    add model spec

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -27,5 +27,26 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Model instantiation' do
+    subject(:new_student) { described_class.new }
+
+    describe 'Database' do
+      it { is_expected.to have_db_column(:id).of_type(:integer) }
+      it { is_expected.to have_db_column(:name).of_type(:string) }
+      it { is_expected.to have_db_column(:email).of_type(:string).with_options(null: false) }
+      it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+      it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+      it { is_expected.to have_db_column(:reset_password_sent_at).of_type(:datetime) }
+      it { is_expected.to have_db_column(:reset_password_token).of_type(:string) }
+    end
+  end
+
+  describe 'Connexion' do
+    it { is_expected.to have_many :attendances }
+    it { is_expected.to have_many :courses }
+  end
+
+  context 'when validation is ok' do
+    it { expect(build(:student)).to be_valid }
+  end
 end
