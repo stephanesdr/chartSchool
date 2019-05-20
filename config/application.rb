0680088707e44bd273262d784f66888300fec5c2
stephanesdr <stadlerstephane@gmail.com>
2019-05-20 20:45:26 +0200

    lint application.rb fix

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module ChartSchool
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
     config.generators do |generators|
       generators.javascripts false
       generators.helper false
