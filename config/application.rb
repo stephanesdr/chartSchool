1740826ae356b299585d55c6d3637767a0bfc318
stephanesdr <stadlerstephane@gmail.com>
2019-05-21 11:07:52 +0200

    lint fix rubocop

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,7 @@ module ChartSchool
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
-    
+
     config.generators do |generators|
       generators.javascripts false
       generators.helper false
