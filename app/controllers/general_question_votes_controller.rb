45a3b966c344fe78b882b6e530de0f23c318d35e
AlexDNZ <paleozene@gmail.com>
2019-05-22 16:20:58 +0200

    student vote conditions in controller /pb gaurd clause

--- app/controllers/general_question_votes_controller.rb
+++ app/controllers/general_question_votes_controller.rb
@@ -3,10 +3,10 @@
 class GeneralQuestionVotesController < ApplicationController
   def create
     student = current_student || Student.last
-    GeneralQuestionVote.create!(
-      general_question_id: params[:general_question_id],
-      student_id: student.id
-    )
+    student_has_voted = GeneralQuestionVote.where(student_id: student.id, general_question_id: params[:general_question_id]).count.zero?
+
+    return GeneralQuestionVote.create!(general_question_id: params[:general_question_id], student_id: student.id) if student_has_voted
+
     redirect_to request.referer
   end
 
