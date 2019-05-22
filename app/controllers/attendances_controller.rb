72ed8088cb8271d31cad11fe0e84b1ba16fc41ab
MargVander <margauxvanderbeken@gmail.com>
2019-05-22 16:45:55 +0200

    modification of create attendance method to include unregistered students

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,9 +2,12 @@
 
 class AttendancesController < ApplicationController
   def create
-    puts params
-    student = Student.find_by(email: params[:email])
-    @attendance = Attendance.create(student: student, course: Course.find(params[:course_id]))
+    if Student.where(email: params[:email]).exists?
+      student = Student.find_by(email: params[:email])
+      @attendance = Attendance.create(student: student, course: Course.find(params[:course_id]))
+    else
+      @attendance = PendingAttendance.create(email: params[:email], course: Course.find(params[:course_id]))
+    end
     redirect_to request.referer
   end
 end
