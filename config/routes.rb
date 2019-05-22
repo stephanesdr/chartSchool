a9ffa89bacd933acf5b6f9b745a0dc9cb0f4967e
redcozmo <redcozmo@hotmail.com>
2019-05-22 11:40:16 +0200

    index in only for courses resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :teachers, path: 'teachers', :controllers => { sessions: "teachers/sessions",
                                                             registrations: "teachers/registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :courses, only: %i[new create show] do
+  resources :courses, only: %i[new create show index] do
     resources :steps, only: %i[create] do
       resources :steps_students, only: %i[create destroy]
     end
