0aac2253704447e427369242e6b9fb3d4c6a294c
MargVander <margauxvanderbeken@gmail.com>
2019-05-24 17:52:14 +0200

    modification of the attendance creation method

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,9 +4,9 @@ class AttendancesController < ApplicationController
   def create
     if Student.where(email: params[:email]).exists?
       student = Student.find_by(email: params[:email])
-      @attendance = Attendance.create(student: student, course: Course.find(params[:course_id]))
+      @attendance = Attendance.create(student: student, course: Course.find(params[:course_id])) unless Attendance.where(student: student, course: Course.find(params[:course_id])).exists?
     else
-      @attendance = PendingAttendance.create(email: params[:email], course: Course.find(params[:course_id]))
+      @attendance = PendingAttendance.create(email: params[:email], course: Course.find(params[:course_id])) unless PendingAttendance.where(email: params[:email], course: Course.find(params[:course_id])).exists?
     end
     redirect_to request.referer
   end
