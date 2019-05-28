51f8afaaf63fa7551826dafd517b4799e01ba4de
MargVander <margauxvanderbeken@gmail.com>
2019-05-28 22:26:02 +0200

    generate new user model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users, path: 'user', :controllers => { registrations: "user/registrations" }
   root to: 'connexion/start#index'
- 
-  namespace :connexion do 
+
+  namespace :connexion do
     resources :start, only: %i[index]
   end
-  
+
   devise_for :students, path: 'students', :controllers => { sessions: "students/sessions",
                                                             registrations: "students/registrations" }
   devise_for :teachers, path: 'teachers', :controllers => { sessions: "teachers/sessions",
@@ -13,13 +14,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :home, only: %i[index]
-  
+
   resources :general_question_votes, only: %i[destroy ]
   resources :general_questions, only: %i[new create index update show destroy] do
     resources :general_question_votes, only: %i[create]
   end
 
-  
+
   resources :courses, only: %i[new create show index] do
     resources :attendances, only: %i[create]
     resources :steps, only: %i[create] do
