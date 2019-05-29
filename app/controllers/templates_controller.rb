680aafe154dd75a4d1953628cd3520a0395dd50d
MargVander <margauxvanderbeken@gmail.com>
2019-05-29 18:44:04 +0200

    generate new controller template with create form and method

---
+++ app/controllers/templates_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class TemplatesController < ApplicationController
+  def new
+    @template = Template.all
+  end
+
+  def create
+    @template = Template.create(title: params[:title], description: params[:description], resource: params[:resource], person: current_person)
+    if @template.errors.any?
+      flash[:warning] = "Echec dans la création du modèle, veuillez à ce que tout les champs soit correctement rempli"
+      redirect_to request.referer
+    elsif flash[:notice] = "Modèle correctement créé"
+      redirect_to root_path
+    end
+  end
+end
