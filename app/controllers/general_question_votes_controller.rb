a2ac788f3a3818ed961fa9d3ed58f78b571dafe5
AlexDNZ <paleozene@gmail.com>
2019-05-30 17:27:31 +0200

    modify general_question_votes_controller.rb replacing student by attendee

--- app/controllers/general_question_votes_controller.rb
+++ app/controllers/general_question_votes_controller.rb
@@ -2,17 +2,17 @@
 
 class GeneralQuestionVotesController < ApplicationController
   def create
-    student = current_student # || Student.last
-    student_hasnot_voted = GeneralQuestionVote.where(student_id: student.id, general_question_id: params[:general_question_id]).count.zero?
+    attendee = current_person
+    attendee_hasnot_voted = GeneralQuestionVote.where(student_id: attendee.id, general_question_id: params[:general_question_id]).count.zero?
 
-    return GeneralQuestionVote.create!(general_question_id: params[:general_question_id], student_id: student.id) if student_hasnot_voted
+    return GeneralQuestionVote.create!(general_question_id: params[:general_question_id], student_id: attendee.id) if attendee_hasnot_voted
 
     # redirect_to request.referer
   end
 
   def destroy
-    student = current_student # || Student.last
-    @vote = GeneralQuestionVote.where(student_id: student.id, general_question_id: params[:id])
+    attendee = current_person
+    @vote = GeneralQuestionVote.where(student_id: attendee.id, general_question_id: params[:id])
 
     GeneralQuestionVote.delete(@vote) if @vote.present?
   end
