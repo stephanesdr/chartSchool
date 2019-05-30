8751756943af74cb1ac3e18311663bcc7dc8dbac
MargVander <margauxvanderbeken@gmail.com>
2019-05-30 20:38:00 +0200

    add time to creation of course through template

--- app/controllers/template_course_controller.rb
+++ app/controllers/template_course_controller.rb
@@ -2,8 +2,10 @@
 
 class TemplateCourseController < ApplicationController
   def create
+    start_time = params[:start_time] + " " + params[:start_hour].first
+    end_time = params[:end_time] + " " + params[:end_hour].first
     template = Template.find(params[:template_id])
-    course = Course.create(title: template.title, description: template.description, resource: template.resource, creator: current_person)
+    course = Course.create(title: template.title, description: template.description, resource: template.resource, creator: current_person, start_time: start_time, end_time: end_time)
     template.step_templates.each do |step|
       Step.create(course: course, title: step.title, description: step.description)
     end
