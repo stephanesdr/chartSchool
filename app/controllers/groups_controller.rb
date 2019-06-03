b3cb406c30bf3cd5a1e5847381a6dbe4eae5e3fd
redcozmo <redcozmo@hotmail.com>
2019-06-03 17:17:59 +0200

    create controller groups with new, create and index methods

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,27 @@
+class GroupsController < ApplicationController
+
+  def new
+    @group = Group.new
+  end
+
+  def index
+    @groups = Group.all
+  end
+
+  def create
+    group = Group.new(group_params)
+    if group.valid?
+      group.save
+      redirect_to group_path(group.id), success: "Groupe créé avec succès"
+    else
+      @group = group
+      render 'new'
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
+end
