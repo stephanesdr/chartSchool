bb9e140c350583f3d00d81dc10a558835b1e10cc
MargVander <margauxvanderbeken@gmail.com>
2019-06-04 00:58:30 +0200

    change before_action to match new db

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,7 +4,7 @@ class AttendancesController < ApplicationController
   def create
     if Person.where(email: params[:email]).exists?
       attendee = Person.find_by(email: params[:email])
-      @attendance = Attendance.create(student: attendee, course: Course.find(params[:course_id])) unless Attendance.where(attendee: attendee, course: Course.find(params[:course_id])).exists?
+      @attendance = Attendance.create(attendee: attendee, course: Course.find(params[:course_id])) unless Attendance.where(attendee: attendee, course: Course.find(params[:course_id])).exists?
     else
       @attendance = PendingAttendance.create(email: params[:email], course: Course.find(params[:course_id])) unless PendingAttendance.where(email: params[:email], course: Course.find(params[:course_id])).exists?
     end
--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,19 +1,7 @@
 # frozen_string_literal: true
 
 module CoursesHelper
-  def no_unregistered_student
-    authenticate_student! unless current_teacher
-  end
-
-  def not_authorized_teacher
-    return unless current_teacher
-
-    redirect_to root_path if current_teacher.id != Course.find(params[:id]).teacher.id
-  end
-
-  def not_authorized_student
-    return unless current_student
-
-    redirect_to root_path unless Attendance.where(course: Course.find(params[:id]), student: current_student).exists?
+  def not_authorized_person
+    redirect_to root_path unless current_person.id == Course.find(params[:id]).creator.id || Attendance.where(course: Course.find(params[:id]), attendee: current_person).exists?
   end
 end
